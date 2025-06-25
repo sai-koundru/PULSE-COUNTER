a42f465eec471c3bd275e7cffde7602ff14a4344
K SREE SAI VENKAT <2003ssv@gmail.com>
2025-06-25 14:33:43 +0530

    Update pulse_counter.v

--- pulse_counter.v
+++ pulse_counter.v
@@ -1,35 +1,42 @@
-module pulse_counter(
+module pulse_counter
+	(
 	    input clk,
 	    input start,
 	    input rst,
+	
 	    output reg op_sig
 	);
 
+	
 reg [3:0] count;
 
+	
 always @(posedge clk or posedge rst)
-begin
-if (rst)
 	begin
-	    count <= 0;
-        op_sig <= 0;
-    end
+		if (rst)
+			begin
+	    			count <= 0;
+        			op_sig <= 0;
+    			end
 else if (start)
 	begin
-	if (count != 9) 
-	    begin
-		count <= count + 1;
-op_sig <= 0;   
-end
-    else
-	    begin
-	    count <= 0;
-op_sig <= 1;  
-end
-end
+		if (count != 9) 
+	    		begin
+				count <= count + 1;
+				op_sig <= 0;   
+			end
+		
+    		else
+	    		begin
+	    			count <= 0;
+				op_sig <= 1;  
+			end
+	end
+		
 else 
 	begin 
-	op_sig <= 0;   
-  end 
+		op_sig <= 0;   
+  	end 
+		
 end
 endmodule
